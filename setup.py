8e408b702b5c3e45ce1852cbe5c477a4fb36ed17
vineethkalimili <119398096+vineethkalimili@users.noreply.github.com>
2023-11-24 10:54:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(name="keras_segmentation",
             ]
       },
       install_requires=[
-            "h5py<=2.10.0",
+            "h5py<=3.9.0",
             "Keras"+keras_ver,
             "imageio==2.5.0",
             "imgaug>=0.4.0",
